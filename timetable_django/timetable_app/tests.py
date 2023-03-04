6307ff4d8e85deea8de5a9eb1a6b9e469c2337dc
i80287 <92759021+i80287@users.noreply.github.com>
2023-03-04 14:35:02 +0300

    update test

--- timetable_django/timetable_app/tests.py
+++ timetable_django/timetable_app/tests.py
@@ -1,12 +1,17 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
+    from rest_framework.response import Response
     pass
 
 from django.test import TestCase
+
+from .models import Category, CommonGroup, Event
 
 # Create your tests here.
 
 class TestTimetable(TestCase):
     def test_index(self) -> None:
-        request = self.client.get("/timetable_app/")
+        request: Response = self.client.get("/api/v1/day/12-12-2023")
+        assert request.status_code == 200
+
--- timetable_django/timetable_app/views.py
+++ timetable_django/timetable_app/views.py
@@ -1,3 +1,8 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from typing import Dict, List
+
 from django.shortcuts import render
 
 from rest_framework.views import APIView
@@ -11,24 +16,21 @@ from datetime import datetime, date
 
 
 class EventDayView(APIView):
-
-    def get(self, request, mydate):
-        start_of_day = datetime.combine(mydate, datetime.min.time())
-        end_of_day = datetime.combine(mydate, datetime.max.time())
+    def get(self, request, mydate) -> Response:
+        start_of_day: datetime = datetime.combine(mydate, datetime.min.time())
+        end_of_day: datetime = datetime.combine(mydate, datetime.max.time())
         events = Event.objects.filter(start__range=(start_of_day, end_of_day))
-        serializer = EventSerializer(events, many=True)
+        serializer: EventSerializer = EventSerializer(events, many=True)
         return Response(serializer.data)
 
 
 class EventWeekView(APIView):
-    def get(self, request, mydate):
+    def get(self, request, mydate) -> Response:
         events = Event.objects.filter(start__week=mydate.isocalendar()[1], start__year=mydate.today().year)
-        response_data = {}
-        for i in range(7):
-            response_data[i] = []
-
+        response_data: Dict[int, List] = {i: [] for i in range(7)}
+        
         for event in events:
-            serializer = EventSerializer(event)
+            serializer: EventSerializer = EventSerializer(event)
             response_data[event.start.weekday()].append(serializer.data)
 
         return Response(response_data)
